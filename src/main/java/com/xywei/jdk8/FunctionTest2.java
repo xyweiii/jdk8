0385c5f0e10539dbaae08561198db733599dbdda
xywei <iterxywei@foxmail.com>
2018-12-26 23:21:20 +0800

    Funtion & BiFunction    compose & andThen

---
+++ src/main/java/com/xywei/jdk8/FunctionTest2.java
@@ -0,0 +1,45 @@
+package com.xywei.jdk8;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class FunctionTest2 {
+
+    public static void main(String[] args) {
+        FunctionTest2 test = new FunctionTest2();
+        System.out.println(test.compute(2, value -> value * 3, value -> value * value));
+        System.out.println(test.compute2(2, value -> value * 3, value -> value * value));
+
+        //BiFunction
+//        System.out.println(test.compute3(1, 2, (a, b) -> a + b));
+//        System.out.println(test.compute3(1, 2, (a, b) -> a - b));
+//        System.out.println(test.compute3(1, 2, (a, b) -> a * b));
+//        System.out.println(test.compute3(1, 2, (a, b) -> a / b));
+
+        System.out.println(test.compute4(2, 3, (a, b) -> a + b, r -> r * r));
+    }
+
+
+    /**
+     * compose
+     */
+    public int compute(int a, Function<Integer, Integer> function1, Function<Integer, Integer> function2) {
+        return function1.compose(function2).apply(a);
+    }
+
+    /**
+     * andThen
+     */
+    public int compute2(int a, Function<Integer, Integer> function1, Function<Integer, Integer> function2) {
+        return function1.andThen(function2).apply(a);
+    }
+
+    public int compute3(int a, int b, BiFunction<Integer, Integer, Integer> biFunction) {
+        return biFunction.apply(a, b);
+    }
+
+    public int compute4(int a, int b, BiFunction<Integer, Integer, Integer> biFunction, Function<Integer, Integer> function) {
+        return biFunction.andThen(function).apply(a, b);
+    }
+
+}
